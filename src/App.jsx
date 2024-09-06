6a6a5b5538aa4f85ca8fe389c42b708dafd413d1
Aditya Jaiswal <jswladitya0403@gmail.com>
2024-09-06 20:59:30 +0530

    save commit

---
+++ src/App.jsx
@@ -0,0 +1,91 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import { TodoProvider } from "./contexts/ToDoContext";
+import TodoForm from "./components/TodoForm";
+import TodoItem from "./components/TodoItem"
+
+function App() {
+  
+  const [todos, setTodos] = useState([]);
+
+  //making of functionality
+  const addTodo = (todo) => {
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
+    
+  };
+
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo))
+    );
+  };
+
+  //delete todo
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((prevTodo) => prevTodo.id !== id));
+  };
+
+  //toggle complete
+  const toggleComplete = (id) => {
+    setTodos((prev) =>
+      prev.map(
+        (prevTodo) =>
+          prevTodo.id === id
+            ? { ...prevTodo, completed: !prevTodo.completed }
+            : prevTodo
+      )
+    );
+  };
+  //till here we have completed the basic functionalities of context
+
+  useEffect(() => {
+    
+    const todos = JSON.parse(localStorage.getItem("todos"));
+
+    if (todos && todos.length > 0) {
+      setTodos(todos);
+    }
+  }, []);
+
+  useEffect(() => {
+  
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+
+  
+  return (
+    <TodoProvider
+      value={{ todos, addTodo, toggleComplete, deleteTodo, updateTodo }}
+    >
+      <div className="bg-[rgb(0,4,8)] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+            Manage Your Todos
+          </h1>
+          <div className="mb-4">
+            <TodoForm />
+          </div>
+          {/* since todo items bhot saare ho sakte he islie usko loop lagake add karenge */}
+          <div className="flex flex-wrap gap-y-3">
+            {todos.map((todo) => (
+              <div key={todo.id} className="w-full">
+                <TodoItem todo={todo} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </TodoProvider>
+  );
+}
+
+export default App;
+
+
+
+//LocalStorage
+//jaise hi app load hoye ho sakta he usme already kuch todos added ho & refresh karne par bhi wahi rahe, to islie inhe browser ke local storage me save karna hota he
+//useEffect, function call karke local storage me jao saari values leke aao jo bhi uske ander he aur unko state wale 'todos' me update kardo
+//
